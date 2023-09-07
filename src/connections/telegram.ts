90395f441f2d7b4234209bd718eeb7d82d9f7aed
ilchu21 <ilchu21@hotmail.com>
2023-09-07 09:35:48 -0400

    telegram polling true

--- src/connections/telegram.ts
+++ src/connections/telegram.ts
@@ -2,4 +2,4 @@ import { TELEGRAM_API_KEY } from '../utils/env-vars';
 import TelegramBot from 'node-telegram-bot-api';
 
 const token: string = TELEGRAM_API_KEY;
-export const bot: TelegramBot = new TelegramBot(token, { polling: false });
+export const bot: TelegramBot = new TelegramBot(token, { polling: true });
